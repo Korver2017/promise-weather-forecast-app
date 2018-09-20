5c60a5d8f52a436e7254d651b99a097e23eae9a9
Korver <korver2017@gmail.com>
2018-09-21 00:14:16 +0800

    change some parts into ES6 syntax

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 // jQuery UI Auto Complete
-let woeidList = {
+const woeidList = {
 	'台北': 2306179,
 	'高雄': 2306180,
 	'台中': 2306181,
@@ -7,7 +7,7 @@ let woeidList = {
 	'花蓮': 2306187,
 	'新北': 2306211
 };
-let woeidListKey = Object.keys(woeidList);
+const woeidListKey = Object.keys(woeidList);
 $(document).ready(function () {
 	$('.form-control').autocomplete({
 		source: woeidListKey
@@ -18,7 +18,7 @@ $(document).ready(function () {
 let info = document.querySelector('.dailyInfo');
 let formControl = document.querySelector('.form-control');
 formControl.addEventListener('keydown', (e) => {
-	if (e.key == 'Enter') {
+	if (e.key === 'Enter') {
 		info.innerHTML = '';
 		let userValue = formControl.value;
 		callAPI(userValue).then(() => {
@@ -46,27 +46,24 @@ let thumbnail = document.querySelector('.thumbnail');
 let callAPI = (location) => {
 	return new Promise((resolve, reject) => {
 		thumbnail.style.display = 'block';
-		let len = woeidListKey.length;
 		document.querySelector('h2').innerHTML = '查詢結果';
 		let result = woeidListKey.filter((key) => {
-			return key == location;
+			return key === location;
 		});
 		console.log(result);
 		if (result.length) {
 			let value = woeidList[result];
-			let data = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(' +
-				value + ')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+			let data = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(${value})&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 			let xhr = new XMLHttpRequest();
 			xhr.open('get', data, true);
 			xhr.send(null);
 			xhr.onload = function () {
 				let json = JSON.parse(xhr.responseText);
-				let city = json.query.results.channel.location.city;
 				let str = '';
 				let image = '';
 				let forecast = json.query.results.channel.item.forecast
-				let forecastLen = forecast.length;
-				for (let i = 0; i < forecastLen; i++) {
+				// let forecastLen = forecast.length;
+				for (let i = 0; i <= 8; i++) {
 					switch (forecast[i].text) {
 						case 'Partly Cloudy':
 							image = '<img src="./img/partly_cloudy.svg">'
@@ -87,10 +84,7 @@ let callAPI = (location) => {
 						default:
 							image = '<img src="./img/no_image.svg">';
 					}
-					str += '<p>' + forecast[i].date + ' ' + forecast[i].day +
-						'<br>' + 'High: ' + forecast[i].high + '°F' +
-						'<br>' + 'Low: ' + forecast[i].low + '°F' +
-						'<br>' + forecast[i].text + image + '</p>';
+					str += `<p>${forecast[i].date} ${forecast[i].day}<br>High: ${forecast[i].high}°F<br>Low: ${forecast[i].low}°F<br>${forecast[i].text}${image}</p>`;
 				};
 				info.innerHTML = str + '<div class="clearfix"></div>';
 			};
